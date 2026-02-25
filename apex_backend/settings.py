779797ee2adfd70438d0d1aff79bac8de9f0a1ff
Emmanuel_Sikwela <sikwelae@gmail.com>
2026-02-25 15:25:08 +0200

    Update settings.py

--- apex_backend/settings.py
+++ apex_backend/settings.py
@@ -91,8 +91,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'home/RikoNaito/apexcreatives_backend/static'
+STATIC_ROOT = 'home/RikoNaito/apexbackend/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/RikoNaito/apexcreatives_backend/media'
+MEDIA_ROOT = '/home/RikoNaito/apexbackend/media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
